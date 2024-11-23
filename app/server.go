94c062f715524bd2098267dfc8596d900c456347
harok <doriantheblue@naver.com>
2024-11-23 22:38:07 +0900

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"net"
 	"os"
@@ -16,40 +15,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	buf := make([]byte, 4096)
-
-	// 계속해서 명령어 읽기
 	for {
-		n, err := conn.Read(buf)
+		buf := make([]byte, 4096)
+		_, err := conn.Read(buf)
 		if err != nil {
 			fmt.Println("Error reading from connection: ", err.Error())
 			return
 		}
-		if n == 0 {
-			continue
-		}
 
-		data := buf[:n]
-
-		// RESP 프로토콜 형식인지 확인
-		if bytes.HasPrefix(data, []byte("*")) {
-			// RESP 프로토콜: 하나의 PONG 응답
-			conn.Write([]byte("+PONG\r\n"))
-		} else {
-			// 일반 텍스트: 줄 단위로 처리
-			commands := bytes.Split(data, []byte("\n"))
-			for _, cmd := range commands {
-				if len(bytes.TrimSpace(cmd)) > 0 {
-					conn.Write([]byte("+PONG\r\n"))
-				}
-			}
-		}
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
